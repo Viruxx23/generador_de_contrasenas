3eb867ab5c914e76cba4843d99528d4c56d65a92
Hernan Exequiel Gramajo <hernangramajo102@gmail.com>
2023-08-02 03:03:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
-"""
-/*
- * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
- * Podrás configurar generar contraseñas con los siguientes parámetros:
- * - Longitud: Entre 8 y 16.
- * - Con o sin letras mayúsculas.
- * - Con o sin números.
- * - Con o sin símbolos.
- * (Pudiendo combinar todos estos parámetros entre ellos)
- */
-"""
-from random import choice
+import random
 import string
 
-def generador_contraseñas(longitud=8, mayusculas=False, numeros=False, simbolos=False):
-    caracteres = string.ascii_letters
-    if longitud< 8 or longitud > 16:
-        print(f"error el tamaño permitido es de 8-16 caracteres")
-        return None
-    if mayusculas:
-        caracteres += string.ascii_uppercase
-    if numeros:
-        caracteres += string.digits
-    if simbolos:
-        caracteres += string.punctuation
+longitud = int(input("ingrese la cantidad de caracteres para la contraseña entre 8 y 16: "))
 
-    # Elegir caracteres aleatorios de la lista de caracteres
-    contraseña = "".join((choice(caracteres)) for i in range(longitud))
-    return contraseña
+numero=input("quieres que incluya numeros? Y/N: ")
+if numero.lower() == "y":
+    numero = True
+elif numero.lower() == "n":
+    numero = False
+else:
+    print(F"parametro no valido")
 
-print(generador_contraseñas(15, True, True, True))
+mayusculas = input("¿Quieres que incluya mayusculas? Y/N: ")
+if mayusculas.lower() == "y":
+    mayusculas = True
+elif mayusculas.lower() == "n":
+    mayusculas = False
+else:
+    print(F"parametro no valido")
+
+simbolos = input("¿Quieres que incluya simbolos? Y/N: ")
+
+def generador_contrasenas(longitud,num,may,sim):
+    if longitud>=8 and longitud<=16:
+        caracteres=[]
+        if num:
+            caracteres+=list(string.digits) #para almazenar los numeros
+        if may:
+            caracteres+=list(string.ascii_uppercase) #para almazenar las mayusculas
+        if sim:
+            caracteres+=list(string.punctuation) #para almazenar los digitos
+        password ="".join(random.choices(caracteres,k=longitud))
+        return password
+    else:
+        print(f"Error debes ingresar una cantidad entre 8 y 16.")
+
+password= generador_contrasenas(longitud=longitud,num=numero,may=mayusculas,sim=simbolos)
+print(password)
+
 
